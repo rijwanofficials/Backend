8c66ae1aaa14548c532900383d573fa59ace391b
Rijwan Husain <husainrijwan2001@gmail.com>
2025-07-24 14:47:01 +0530

    external modules in NodeJs

---
+++ Day05/External Modules/app.js
@@ -0,0 +1,12 @@
+import chalk from 'chalk';
+
+const name1 = chalk.red("Namer");
+const org1 = chalk.magenta("Rizwan")
+console.log(name1, org1);
+
+const name2 = chalk.green("organization")
+const org2 = chalk.magentaBright
+("Programming pathshala")
+
+console.log(name2, org2);
+
