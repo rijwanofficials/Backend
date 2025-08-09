42150d30bf977eed60186f2f31dfb65feb9db9e2
Rijwan Husain <husainrijwan2001@gmail.com>
2025-08-10 02:08:42 +0530

    Update and Delete using mongoose

--- EXPRESS/Day5/api/v1/product/controllers.js
+++ EXPRESS/Day5/api/v1/product/controllers.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const { ProductModel } = require('../../../models/productSchema');
+const { validateObjectId } = require("../../../utils/validateObjectId");
 
+// CREATE Product 
 const createProductController = async (req, res) => {
     try {
         console.log("-----Inside createProductController-----");
@@ -32,8 +34,9 @@ const createProductController = async (req, res) => {
     }
 }
 
+// GET Product 
 const getAllProductController = async (req, res) => {
-    console.log("-----Inside getAllProductController-----");
+    console.log("<-----Inside getAllProductController------>");
     try {
         const products = await ProductModel.find();
         res.status(200).json({
@@ -44,11 +47,75 @@ const getAllProductController = async (req, res) => {
             }
         });
     } catch (err) {
+        console.log("----Error inside getAllProductController--->>", err.message);
         res.status(500).json({
             isSuccess: false,
             message: "Failed to fetch products",
             error: err.message
         });
+    };
+};
+
+// EDIT Product 
+const editProductController = async (req, res) => {
+    console.log("<-----Inside editProductController------>");
+    const { id } = req.params;
+
+    // Validate before proceeding
+    if (!validateObjectId(id, res)) return;
+    try {
+        const updatedProduct = await ProductModel.findByIdAndUpdate(
+            id,
+            req.body,
+            { new: true, runValidators: true }
+        ).lean();
+        if (!updatedProduct) {
+            return res.status(404).json({
+                isSuccess: false,
+                message: "Product not found"
+            });
+        }
+        res.status(200).json({
+            isSuccess: true,
+            message: "Product updated successfully",
+            data: { product: updatedProduct }
+        })
+    } catch (err) {
+        console.log("----Error inside editProductController--->>", err.message);
+        res.status(500).json({
+            isSuccess: false,
+            message: "Failed to update product",
+            error: err.message
+        });
     }
-}
-module.exports = { createProductController, getAllProductController }
+};
+
+// DELETE Product 
+const deleteProductController = async (req, res) => {
+    console.log("<-----Inside deleteProductController------>");
+    const { id } = req.params;
+
+    // Validate before proceeding
+    if (!validateObjectId(id, res)) return;
+    try {
+        const product = await ProductModel.findByIdAndDelete(id);
+        if (!product) {
+            return res.status(404).json({
+                isSuccess: false,
+                message: "Product not found"
+            });
+        }
+        res.status(200).json({
+            isSuccess: true,
+            message: "Product deleted successfully",
+        })
+    } catch (err) {
+        console.log("----Error inside deleteProductController--->>", err.message);
+        res.status(500).json({
+            isSuccess: false,
+            message: "Failed to delete product",
+            error: err.message
+        });
+    }
+};
+module.exports = { createProductController, getAllProductController, editProductController, deleteProductController }
